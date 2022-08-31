5496394d7339317f16d7dd698341a13ec9739ec3
dgraham742 <106363652+dgraham742@users.noreply.github.com>
2022-08-31 07:36:45 -0500

    JS test finished

--- move.js
+++ move.js
@@ -6,9 +6,6 @@ var typeBtn = document.querySelector("#ser")
 var controlBtn = document.querySelector("#control")
 //function for country of origin
 var countryOfOriginBtn = document.querySelector("#origin")
-//to do-need an API for Country of Origin
-var originOfName = "https://gender-api.com/v2/country-of-origin.json"
-var apiKey = "d3ea58ecf9205655990f1cf8327310516bbb1a3f0c4eb64813cb698bbddafd09"
 var submitBtn = document.querySelector("#fetchBtn")
 var nameGen = document.querySelector("per")
 var male = document.querySelector("#answer")
@@ -16,18 +13,14 @@ var female = document.querySelector("#answer")
 var neutral = document.querySelector("#answer")
 
 //fetch female name APIs
-// fetch ("https://www.randomlists.com/data/names-female.json")
-// .then((Response) =>Response.json())
-// .then((data) =>console.log(data));
+fetch ("https://www.randomlists.com/data/names-female.json")
+.then((Response) =>Response.json())
+.then((data) =>console.log(data));
 
-// //fetch Male name APIs
-// fetch ('http://www.randomlists.com/data/names-male.json')
-// .then((Response) =>Response.json())
-// .then((data) =>console.log(data));
-// //fetch country of origin API
-// fetch ('https://gender-api.com/v2/country-of-origin ')
-// .then((Response) =>Response.json())
-// .then((data) =>console.log(data));
+//fetch Male name APIs
+fetch ('http://www.randomlists.com/data/names-male.json')
+.then((Response) =>Response.json())
+.then((data) =>console.log(data));
 
 function generateRandomName() {
     console.log("generate random name")
@@ -81,3 +74,17 @@ for (let i= 0; index < lengthInput; i++) {
     
 }
 generateRandomName()
+
+function submitBtn() {
+    console.log("Submit button")
+    Element.addEventListner("click",submitBtn);
+    return window.alert("BUTTON WAS CLICKED")
+
+}    
+submitBtn()
+
+function generateName() {
+    femaleNameUrl ()
+    maleNameUrl  ()
+}
+submitBtn.addEventListner("click",generateName)
